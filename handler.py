42537fa6ed1b78368189bca4e9f4c53ac43e7728
essket <essket88@gmail.com>
2025-08-18 20:54:31 +0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,18 +1,5 @@
-"""Example handler file."""
-
-import runpod
-
-# If your handler runs inference on a model, load the model here.
-# You will want models to be loaded into memory before starting serverless.
-
-
-def handler(job):
-    """Handler function that will be used to process jobs."""
-    job_input = job["input"]
-
-    name = job_input.get("name", "World")
-
-    return f"Hello, {name}!"
-
-
-runpod.serverless.start({"handler": handler})
+# src/handler.py
+def handler(event):
+    # event["input"] — это то, что ты пришлёшь в запросе
+    name = (event.get("input") or {}).get("name", "world")
+    return {"ok": True, "msg": f"Hello, {name}! Serverless работает."}
